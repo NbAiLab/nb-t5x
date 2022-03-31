18ddfcaabd62c07d2ed607772e76f19dfd817422
Javier de la Rosa <versae@gmail.com>
2022-03-31 13:02:59 +0200

    Clean up tasks file

--- tasks.py
+++ tasks.py
@@ -1,51 +1,41 @@
-"""Add Tasks to registry."""
 import functools
 
 import seqio
+import tensorflow as tf
 import t5.data
 from datasets import load_dataset
 from t5.data import postprocessors
 from t5.data import preprocessors
 from t5.evaluation import metrics
-#import tensorflow as tf
-import tensorflow.compat.v2 as tf
-import tensorflow_datasets as tfds
+from seqio import FunctionDataSource, utils
 
 TaskRegistry = seqio.TaskRegistry
 
+DATASET_NAME = 'NbAiLab/NCC'
 DEFAULT_OUTPUT_FEATURES = {
-#    "inputs": seqio.Feature(
-#        vocabulary=t5.data.get_default_vocabulary(), add_eos=True,
-#        required=False),
+    "inputs": seqio.Feature(
+        vocabulary=t5.data.get_default_vocabulary(), add_eos=True,
+        required=False),
     "targets": seqio.Feature(
         vocabulary=t5.data.get_default_vocabulary(), add_eos=True)
 }
 
 
-
-from seqio import FunctionDataSource, utils
-
-# ncc = load_dataset('NbAiLab/NCC')
-
-def gen_ncc(split):
-    ncc = load_dataset('NbAiLab/NCC')
+def gen_dataset(split, column="text"):
+    dataset = load_dataset(DATASET_NAME)
     while True:
-        for item in ncc[str(split)]:
-            #print(item["text"])
-            yield item["text"]
+        for item in dataset[str(split)]:
+            yield item[column]
 
 
 def dataset_fn(split, shuffle_files, seed=None):
-    #shape = {'train': 20830348, 'validation': 473079}
-    return tf.data.Dataset.from_generator(functools.partial(gen_ncc, split), output_signature=tf.TensorSpec(shape=(), dtype=tf.string, name="NCC"))
+    return tf.data.Dataset.from_generator(functools.partial(gen_dataset, split), output_signature=tf.TensorSpec(shape=(), dtype=tf.string, name=DATASET_NAME))
 
 
 @utils.map_over_dataset
-def value_to_dict(x, key_map, value_key):
-    """Assign the value from the dataset to value_key in key_map"""
-    # print(type(x), x, str(x))
-    key_map[value_key] = str(x)  # .numpy().decode("utf8")
-    return key_map
+def target_to_key(x, key_map, target_key):
+    """Assign the value from the dataset to target_key in key_map"""
+    return {**key_map, target_key: x}
 
 
 # ==================================== C4 ======================================
@@ -55,18 +45,15 @@ TaskRegistry.add(
     source=seqio.FunctionDataSource(dataset_fn=dataset_fn, splits=("train", "validation"), caching_permitted=False),
     preprocessors=[
         functools.partial(
-            value_to_dict, key_map={
+            target_to_key, key_map={
                 "inputs": None,
                 "targets": None,
-            }, value_key="targets"),
+            }, target_key="targets"),
         seqio.preprocessors.tokenize,
         #seqio.CacheDatasetPlaceholder(),
         preprocessors.span_corruption,
         seqio.preprocessors.append_eos_after_trim,
-
     ],
-    output_features=DEFAULT_OUTPUT_FEATURES,
-#    output_features=seqio.Feature(
-#        vocabulary=t5.data.get_default_vocabulary(), add_eos=True),
+    output_features={"targets": DEFAULT_OUTPUT_FEATURES["targets"]},
     metric_fns=[]
 )

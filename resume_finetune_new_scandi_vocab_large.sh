0393f35897d24996a3b4b6260d04660bb82a497b
Javier de la Rosa <versae@gmail.com>
2022-10-20 08:56:08 +0000

    Add resume script

--- resume_finetune_new_scandi_vocab_large.sh
+++ resume_finetune_new_scandi_vocab_large.sh
@@ -1,11 +1,14 @@
 MODEL_DIR="gs://nb-t5/t5/t5_1_1_large_from_t5_scandi_unigram"
 
 echo "Retrieving latest checkpoint..."
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/google-cloud-sdk/bin
+HOME=/home/javierr
+BOTO_CONFIG="/etc/boto.cfg"
 LAST_CHECKPOINT=$(gsutil ls -l $MODEL_DIR/checkpoint_*/ | sort -k 2 | tail -2 | head -1 | cut -d " " -f 8)
-INITIAL_CHECKPOINT=$(python -c "print('$LAST_CHECKPOINT'.rsplit('/', 1)[0])");
+INITIAL_CHECKPOINT=$(/data/venvt5/bin/python -c "print('$LAST_CHECKPOINT'.rsplit('/', 1)[0])");
 echo "Starting from checkpoint $INITIAL_CHECKPOINT"
 
-python -m t5x.train \
+/data/venvt5/bin/python -m t5x.train \
   --gin_file=finetune_t5_large_new_scandi_vocab.gin \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.INITIAL_CHECKPOINT_PATH=\"${INITIAL_CHECKPOINT}\" \
